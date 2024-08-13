2579fa25f4d0665cc86ecda9ba24728871bbd2d7
Ayesha1250 <muizasif28@gmail.com>
2024-08-13 13:58:28 +0500

    first project next js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return(
     <div><figure className="md:flex bg-red-300 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-    <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/ayesha-pic.jpg" alt="" width="384" height="5.5"/>
+    <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/image.jpg" alt="" width="261" height="500"/>
     <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
      <blockquote>
       <p className="text-lg font-medium">
